691a6941bde7ac9ef0c6d2542fd1c6c4b998e164
Dispatch <dispatch@cartercodes.dev>
2026-04-04 21:11:29 -0400

    Site deployment

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/lsb-hodge-roofing-and-remodeling-llc-gastonia-34x8m9-gntflzc8t',
+  images: { unoptimized: true },
+};
+module.exports = nextConfig;
